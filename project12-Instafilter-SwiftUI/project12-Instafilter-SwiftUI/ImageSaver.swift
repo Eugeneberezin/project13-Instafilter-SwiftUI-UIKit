d1895ccb19f630875b18035789fc6f9c249157a0
Eugene Berezin <berezin.nlp@gmail.com>
2020-05-30 13:15:28 -0700

    finished project

---
+++ project12-Instafilter-SwiftUI/project12-Instafilter-SwiftUI/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  project12-Instafilter-SwiftUI
+//
+//  Created by Eugene Berezin on 5/29/20.
+//  Copyright © 2020 Eugene Berezin. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
